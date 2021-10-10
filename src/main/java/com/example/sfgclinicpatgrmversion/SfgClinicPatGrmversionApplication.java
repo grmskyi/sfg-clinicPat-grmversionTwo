1725919eafa3f3fc2f0e20e3773012a9317677fa
grmskyi <grymovskyz@gmail.com>
2021-10-10 19:07:55 +0300

    Initial commit

---
+++ src/main/java/com/example/sfgclinicpatgrmversion/SfgClinicPatGrmversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgclinicpatgrmversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgClinicPatGrmversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgClinicPatGrmversionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfgclinicpatgrmversion/SfgClinicPatGrmversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgclinicpatgrmversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgClinicPatGrmversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
